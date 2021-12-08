284a1a6c1432f13a6e8ba5f2cf7cda3e429eb83a
yashshingade28 <88716866+yashshingade28@users.noreply.github.com>
2021-12-08 22:32:46 +0530

    Update eqnsolver(binsearch).cpp

--- eqnsolver(binsearch).cpp
+++ eqnsolver(binsearch).cpp
@@ -1,30 +1,34 @@
-// code to solve the equation x*(x+1) = n for x given n .
-
-
-ll n , x;
-cin >> n;
-
-// here l and r are the strict lower and upper bounds of the root x .
-ll l = 1, r = 1e9;
-
-while (r - l > 1)
-{
-    x = (l + r) / 2;
-    if (x * (x + 1) == n)
+// code to solve the equation x*x = n for x given n .
+class eqnsolver {
+private:
+    long long eqn(long long x)
     {
-        l = x;
-        r = x;
-        break;
+        return x * x;
     }
-    else if (x * (x + 1) < n)
+
+public:
+    // this function finds the root of eqn(x) = n from l+1 to r-1 . 
+    pair<long long , long long> solve(long long n)
     {
-        l = x;
+        long long x , l = -1 , r = 3e9 + 1;
+        while (r - l > 1)
+        {
+            x = (l + r) / 2;
+            if (eqn(x) < n)
+            {
+                l = x;
+            }
+            else if (eqn(x) > n)
+            {
+                r = x;
+            }
+            else
+            {
+                return {x , x};
+            }
+        }
+        return {l , r};
     }
-    else if (x * (x + 1) > n)
-    {
-        r = x;
-    }
-}
-// At this point in the code
+};
 // if the root of the equation is integer then l==r==root
 // else the root lies within (l,r) where r == l+1 
