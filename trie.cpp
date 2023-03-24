07feed457fcd7966b0b85eeda4f6140d1f59a2cc
Yash Shingade <88716866+yashshingade28@users.noreply.github.com>
2023-03-25 01:23:57 +0530

    added erase function

--- trie.cpp
+++ trie.cpp
@@ -1,15 +1,15 @@
 class trie {
-    class node {
-    public:
+   private:
+    struct node {
         std::unordered_map<char, node*> children;
-        int noofends , noofpasses;
+        int noofends, noofpasses;
 
-        node() : noofends(0) , noofpasses(0) {}
+        node() : noofends(0), noofpasses(0) {}
     };
 
     node* root;
 
-public:
+   public:
     trie() { root = new node(); }
 
     void insert(const std::string& word) {
@@ -24,7 +24,7 @@ public:
         current->noofends += 1;
     }
 
-    bool search(const std::string& word) {
+    bool find(const std::string& word) {
         node* current = root;
         for (char c : word) {
             if (current->children.find(c) == current->children.end()) {
@@ -35,13 +35,39 @@ public:
         return (current->noofends > 0);
     }
 
-    void extract(const std::string& word) {
-        assert(search(word) == true);
+    bool extract(const std::string& word) {
+        if (!find(word)) return false;
         node* current = root;
         for (char c : word) {
             current = current->children[c];
             current->noofpasses -= 1;
         }
         current->noofends -= 1;
+        return true;
+    }
+
+    bool erase(const std::string& word) {
+        node* current = root;
+        std::stack<std::pair<node*, char>> backtrack;
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                return false;
+            }
+            backtrack.push(std::make_pair(current, c));
+            current = current->children[c];
+        }
+        if (current->noofends == 0) {
+            return false;
+        }
+        current->noofends -= 1;
+        while (!backtrack.empty()) {
+            auto [parent, c] = backtrack.top();
+            backtrack.pop();
+            parent->children[c]->noofpasses -= 1;
+            if (parent->children[c]->noofpasses == 0) {
+                parent->children.erase(c);
+            }
+        }
+        return true;
     }
 };
