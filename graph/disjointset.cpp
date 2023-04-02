8483dc8ac35a841eabe2fc1149593dd6de0f223d
Yash Shingade <88716866+yashshingade28@users.noreply.github.com>
2023-04-03 00:07:24 +0530

    Update and rename disjointset.cpp to graph/disjointset.cpp

--- graph/disjointset.cpp
+++ graph/disjointset.cpp
@@ -1,22 +1,18 @@
 class dsu {
-public:
+   public:
     int n;
     vector<int> p , sz ;
-
-    dsu(int k) : p(n), sz(n, 1), n(k) {
+    dsu(int k) : n(k), p(n), sz(n, 1) {
         iota(p.begin(), p.end(), 0);
     }
-
     int find(int x) {
         return (x == p[x] ? x : (p[x] = find(p[x])));
     }
-
     bool merge(int x, int y) {
         x = find(x);
         y = find(y);
         if (x != y) {
-            if (sz[x] < sz[y])
-                swap(x, y);
+            if (sz[x] < sz[y]) swap(x, y);
             p[y] = x;
             sz[x] += sz[y];
             sz[y] = 0 ;
